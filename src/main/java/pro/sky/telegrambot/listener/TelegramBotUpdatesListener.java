abc9c3e3e6c137a4837c37ac856e1f222ed497f8
action0709 <action0709@mail.ru>
2023-10-09 10:35:27 +0400

    часть 1 +частично 2

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,21 +3,33 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.entity.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.aspectj.weaver.patterns.ISignaturePattern.PATTERN;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-
+public static final Pattern PATTERN=Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
+    @Autowired
+    private NotificationTaskRepository repository;
 
     @PostConstruct
     public void init() {
@@ -27,8 +39,21 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
+            String text = update.message().text();
+            Long chatId=update.message().chat().id();
+            Matcher matcher = PATTERN.matcher(text);
+            if ("/start".equalsIgnoreCase(text)){
+                telegramBot.execute(new SendMessage(chatId, "Привет!"));
+            }else if (matcher.matches()){
+               String time= matcher.group(1);
+                LocalDateTime execDate = LocalDateTime
+                        .parse(time, FORMATTER);
+                NotificationTask task = new NotificationTask();
+                task.setChatId(chatId);
+                task.setText(matcher.group(3));
+                task.setExecDate(execDate);
+                repository.save(task);
+                }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
